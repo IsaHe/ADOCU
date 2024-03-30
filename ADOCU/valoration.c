fe867a592b39aa8acf8978f2f63393d15fd272c3
Isaac Esteban <isaacherbozo06@gmail.com>
2024-03-30 19:37:20 +0100

    Unirse al grupo (refactor Revisado)

--- ADOCU/valoration.c
+++
@@ -1,9 +0,0 @@
-#include "valoration.h"
-#include <stdio.h>
-
-//Crear valoracion por un char pasado
-Valoration insertIntoValoration(char val){
-    Valoration v;
-    v.val = val;
-    return v;
-}
